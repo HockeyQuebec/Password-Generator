3068d61be33696a4a2674433f2b08da57e9e8988
Parker Pulfer <>
2020-09-16 13:54:43 -0400

    Got multithreading to work!!!!!!

--- password_generator.py
+++ password_generator.py
@@ -20,10 +20,6 @@ password_greek_symbols = ["α", "β", "γ", "δ", "ε", "ζ", "η", "θ", "ι", 
 
 password_greek_symbols_caps = ["Α", "Β", "Γ", "Δ", "Ε", "Ζ", "Η", "Θ", "Ι", "Κ", "Λ", "Μ", "Ν", "Ξ", "Ο", "Π", 
                                 "Ρ", "Σ", "Τ", "Υ", "Φ", "Χ", "Ψ", "Ω"]
-
- #= ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "j" , "k" , "l" , "m" , "n" , "o" , 
-#"p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
-
 password_all_characters = [""]
 
 print("Please respond with y or n")
@@ -32,57 +28,118 @@ althabet = input("would you like the alphabet?")
 if althabet.__contains__("y") == True:
    password_all_characters = password_all_characters + password_alphabet
 
+
 q_althabet_caps = input("would you like caps alphabet?")
 if q_althabet_caps.__contains__("y") == True:
     password_all_characters = password_all_characters + password_alphabet_caps
+
 
 symbols = input("would you like symbols?")
 if symbols.__contains__("y") == True:
     password_all_characters = password_all_characters + password_symbols
 
+
 numbers = input("would you like numbers?")
 if numbers.__contains__("y") == True:
     password_all_characters = password_all_characters + password_numbers
+
 
 q_greek_althabet = input("would you like the greek alphabet?")
 if q_greek_althabet.__contains__("y") == True:
     password_all_characters = password_all_characters + password_greek_symbols
 
+
 q_greek_althabet_caps = input("would you like caps greek alphabet?")
 if q_greek_althabet_caps.__contains__("y") == True:
     password_all_characters = password_all_characters + password_greek_symbols_caps
 
-password_length = int (input("length"))
-password_length = int(password_length)
-password_length = password_length//2
-print(password_length)
 
+password_length = int (input("length"))
+
+password_length = int(password_length) 
+
+password_length_1 = password_length//12
+
+password_length_help = password_length_1*12
+
+corrrect = password_length - password_length_help
 
 generated_password = ""
 
-def task1():
-    global generated_password
-    for i in range(0, password_length):
-        n = random.randint(0,len(password_all_characters)-1)
-        generated_password = generated_password + password_all_characters[n]
-        print(generated_password)
 
 def task2():
     global generated_password
-    for i in range(0, password_length):
+    for i in range(0, password_length_1):
         h = random.randint(0,len(password_all_characters)-1)
         generated_password = generated_password + password_all_characters[h]
-        print(generated_password)
 
-t1 = threading.Thread(target=task2, name=task1)
-t2 = threading.Thread(target=task2, name=task2)
+def task3():
+    global generated_password
+    for i in range(0, password_length_1):
+        f = random.randint(0,len(password_all_characters)-1)
+        generated_password = generated_password + password_all_characters[f]
+
+
+
+
+t1 = threading.Thread(target=task2, name="task1")
+
+t2 = threading.Thread(target=task2, name="task2")
+
+t3 = threading.Thread(target=task2, name="task3")
+
+t4 = threading.Thread(target=task2, name="task4")
+
+t5 = threading.Thread(target=task2, name="task5")
+
+t6 = threading.Thread(target=task2, name="task6")
+
+t7 = threading.Thread(target=task3, name="task7")
+
+t8 = threading.Thread(target=task3, name="task8")
+
+t9 = threading.Thread(target=task3, name="task9")
+
+t10 = threading.Thread(target=task3, name="task10")
+
+t11 = threading.Thread(target=task3, name="task11")
+
+t12 = threading.Thread(target=task3, name="task12")
+
 
 t1.start()
 t2.start()
+t3.start()
+t4.start()
+t5.start()
+t6.start()
+t7.start()
+t8.start()
+t9.start()
+t10.start()
+t11.start()
+t12.start()
 
 
 t1.join() 
-t2.join() 
+t2.join()
+t3.join() 
+t4.join()
+t5.join() 
+t6.join()
+t7.join() 
+t8.join()
+t9.join() 
+t10.join()
+t11.join() 
+t12.join()
+
+print(corrrect)
+for i in range(0, corrrect):
+    t = random.randint(0,len(password_all_characters)-1)
+    generated_password = generated_password + password_all_characters[t]
+
+
 print(generated_password)
 
 
