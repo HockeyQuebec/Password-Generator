46c3c12538aef892aae569d48019215a3fb25c41
Parker Pulfer <>
2020-09-23 18:11:01 -0400

    Got tread worker to work.

--- password_generator.py
+++ password_generator.py
@@ -22,123 +22,39 @@ password_greek_symbols_caps = ["Α", "Β", "Γ", "Δ", "Ε", "Ζ", "Η", "Θ", "
                                 "Ρ", "Σ", "Τ", "Υ", "Φ", "Χ", "Ψ", "Ω"]
 password_all_characters = [""]
 
-print("Please respond with y or n")
-while ex==1:
+#
+# checks if workers are running and when they have finished prints the generated password
+#
+def control_task():
+    while True:
+        worker_threads_running = 0
+        for thread in threading.enumerate():
+            print("Thread " + thread.name + " isAlive " + str(thread.is_alive()) + "\n")
+            if thread.name.startswith("worker"):
+                worker_threads_running = worker_threads_running + 1
 
-    # Desides if you want the alphabet
-    althabet = input("Would you like the alphabet?:")
-    if althabet.__contains__("y") == True:
-        password_all_characters = password_all_characters + password_alphabet
+        if worker_threads_running == 0:
+            print("All threads have finished")
+            finish()
+            exit()
 
-    # Desides if you want the alphabet in capitals
-    q_althabet_caps = input("Would you like caps alphabet?:")
-    if q_althabet_caps.__contains__("y") == True:
-        password_all_characters = password_all_characters + password_alphabet_caps
+        sleep(3.0)
 
-    # Desides if you want symbols
-    symbols = input("Would you like symbols?:")
-    if symbols.__contains__("y") == True:
-        password_all_characters = password_all_characters + password_symbols
+#
+# worker that will generate part of the password
+#
+def password_worker_task():
+    global generated_password
+    for i in range(0, password_length_partions):
+        h = random.randint(0,len(password_all_characters)-1)
+        generated_password = generated_password + password_all_characters[h]
+        if i % 10000 == 0:
+            print(threading.current_thread.__name__ + " iteration [" + str(i) + "] of [" + str(password_length_partions) + "]\n")
 
-    # Desides if you want numbers
-    numbers = input("Would you like numbers?:")
-    if numbers.__contains__("y") == True:
-        password_all_characters = password_all_characters + password_numbers
-
-    # Desides if you want the greek alphabet
-    q_greek_althabet = input("Would you like the greek alphabet?:")
-    if q_greek_althabet.__contains__("y") == True:
-        password_all_characters = password_all_characters + password_greek_symbols
-
-    # Desides if you want the greek alphabet in capitals
-    q_greek_althabet_caps = input("Would you like caps greek alphabet?:")
-    if q_greek_althabet_caps.__contains__("y") == True:
-        password_all_characters = password_all_characters + password_greek_symbols_caps
-
-    # Gets user input how long they want the password
-    password_length = int(input("length"))
-
-    # create work for each thread rounding down
-    password_length_partions = password_length//12
-
-    # makes the partions back up to the highest number that is lowwer than the inputed value.
-    password_length_help = password_length_partions*12
-
-    # figures out how much extra charecters that are needed
-    corrrect = password_length - password_length_help
-
-    # Defines that the generated password is a string
-    generated_password = ""
-
-    # created two tasks because 12 threads on a single funtion is too much. It doesn't have all 12 working at the same time. Both tasks are the same.  
-    def task2():
-        global generated_password
-        for i in range(0, password_length_partions):
-            h = random.randint(0,len(password_all_characters)-1)
-            generated_password = generated_password + password_all_characters[h]
-
-    def task3():
-        global generated_password
-        for i in range(0, password_length_partions):
-            f = random.randint(0,len(password_all_characters)-1)
-            generated_password = generated_password + password_all_characters[f]
-
-
-
-    # Defines all of the threads
-
-    t1 = threading.Thread(target=task2, name="task1")
-
-    t2 = threading.Thread(target=task2, name="task2")
-
-    t3 = threading.Thread(target=task2, name="task3")
-
-    t4 = threading.Thread(target=task2, name="task4")
-
-    t5 = threading.Thread(target=task2, name="task5")
-
-    t6 = threading.Thread(target=task2, name="task6")
-
-    t7 = threading.Thread(target=task3, name="task7")
-
-    t8 = threading.Thread(target=task3, name="task8")
-
-    t9 = threading.Thread(target=task3, name="task9")
-
-    t10 = threading.Thread(target=task3, name="task10")
-
-    t11 = threading.Thread(target=task3, name="task11")
-
-    t12 = threading.Thread(target=task3, name="task12")
-
-    # Start all of the threads
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-    t6.start()
-    t7.start()
-    t8.start()
-    t9.start()
-    t10.start()
-    t11.start()
-    t12.start()
-
-    # Waits for the threads to do their work
-    t1.join() 
-    t2.join()
-    t3.join() 
-    t4.join()
-    t5.join() 
-    t6.join()
-    t7.join() 
-    t8.join()
-    t9.join() 
-    t10.join()
-    t11.join() 
-    t12.join()
-
+#
+# print the password and ask if they want to save to file
+#
+def finish(generated_password):
     # Generates all of the extra charaters
     for i in range(0, corrrect):
         t = random.randint(0,len(password_all_characters)-1)
@@ -177,4 +93,67 @@ while ex==1:
 
     # This takes user input to deside if you want to exit
     if v.__contains__("y") == True:
-        ex = 0
+        ex = 0   
+
+print("Please respond with y or n")
+while ex==1:
+
+    # Desides if you want the alphabet
+    althabet = input("Would you like the alphabet?:")
+    if althabet.__contains__("y") == True:
+        password_all_characters = password_all_characters + password_alphabet
+
+    # Desides if you want the alphabet in capitals
+    q_althabet_caps = input("Would you like caps alphabet?:")
+    if q_althabet_caps.__contains__("y") == True:
+        password_all_characters = password_all_characters + password_alphabet_caps
+
+    # Desides if you want symbols
+    symbols = input("Would you like symbols?:")
+    if symbols.__contains__("y") == True:
+        password_all_characters = password_all_characters + password_symbols
+
+    # Desides if you want numbers
+    numbers = input("Would you like numbers?:")
+    if numbers.__contains__("y") == True:
+        password_all_characters = password_all_characters + password_numbers
+
+    # Desides if you want the greek alphabet
+    q_greek_althabet = input("Would you like the greek alphabet?:")
+    if q_greek_althabet.__contains__("y") == True:
+        password_all_characters = password_all_characters + password_greek_symbols
+
+    # Desides if you want the greek alphabet in capitals
+    q_greek_althabet_caps = input("Would you like caps greek alphabet?:")
+    if q_greek_althabet_caps.__contains__("y") == True:
+        password_all_characters = password_all_characters + password_greek_symbols_caps
+
+    # Gets user input how long they want the password
+    password_length = int(input("length"))
+    threads = input("How many threads do you want?:")
+    threads = int(threads)
+    # create work for each thread rounding down
+    password_length_partions = password_length//12
+
+    # makes the partions back up to the highest number that is lowwer than the inputed value.
+    password_length_help = password_length_partions*12
+
+    # figures out how much extra charecters that are needed
+    corrrect = password_length - password_length_help
+
+    # Defines that the generated password is a string
+    generated_password = ""
+
+   
+    # Defines all of the threads
+    for i in range(0, threads):
+        name = "worker " + str(i)
+        thread = threading.Thread(target=password_worker_task, name=name)
+        thread.daemon = True
+        thread.start()
+
+    # start the main control thread that will output the password once all the threads have finished
+    thread = threading.Thread(target=control_task, name="main control")
+    thread.daemon = True
+    thread.start()
+
